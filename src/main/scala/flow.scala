272a6829045904d63321a522b941657ab0871bd1
Chris Hodapp <chodapp@rocketlawyer.com>
2016-02-01 03:09:38 -0800

    A few comments

--- src/main/scala/flow.scala
+++ src/main/scala/flow.scala
@@ -164,21 +164,6 @@ class FlowMacros(val c: blackbox.Context){
   val pkg = q"org.cvogt.flow.`package`"
   val conv = q"$pkg.omitFlowContextMonad"
 
-  // object RemoveFlowContextTypeAnnotations extends Transformer {
-  //   override def transform(tree: Tree) = {
-  //     val t = tree match {
-  //       case q"$mods val $name : $ttree = $_" if ttree.symbol == weakTypeOf[FlowContext].typeSymbol =>
-  //         q"$mods val $name : ${TypeTree()}"
-  //       case q"$lhsT[..$ttrees](...$argsT)" if ttrees.map(_.symbol).contains( weakTypeOf[FlowContext].typeSymbol ) =>
-  //         q"$lhsT(...$argsT)"
-  //       case q"$lhsT.$rhsT[..$ttrees](...$argsT)" if ttrees.map(_.symbol).contains( weakTypeOf[FlowContext].typeSymbol ) =>
-  //         q"$lhsT.$rhsT(...$argsT)"
-  //       case other => other
-  //     }
-  //     super.transform(t)
-  //   }
-  // }
-
   class Transformations(
     contextName: TermName,
     M: Type,
@@ -207,11 +192,13 @@ class FlowMacros(val c: blackbox.Context){
       override def transform(t: Tree) = t
     }
 
+    // Untypecheck the tree
     def untypecheck(next: Transformation) =
       Transformation("untypecheck") { t =>
         next.transform(c.untypecheck(t))
       }
 
+    // Implicitly extract contextual values in statement position
     def doImplicitExtracts(next: Transformation) =
       Transformation("implicit extracts") { t =>
         def doTransformation(
@@ -244,6 +231,7 @@ class FlowMacros(val c: blackbox.Context){
         // For now, we'll ignore local classes and traits, but we should support them
         case class DefineClass(defined: ClassDef) extends ScopeOp
 
+        // Comprehensive comprehensions
         def doContextTransform(
           trees: List[Tree],
           scopeOps: List[ScopeOp],
@@ -334,6 +322,7 @@ class FlowMacros(val c: blackbox.Context){
       def unify: Tree = q"..$t"
     }
 
+    // Puts things into form for the extractions phase.
     def normalize(next: Transformation): Transformation = Transformation("normalize") { t =>
 
       def doNormalize(pending: List[Tree], done: List[Tree]): Tree = pending match {
@@ -423,6 +412,7 @@ class FlowMacros(val c: blackbox.Context){
       next.transform(doNormalize(t.shard, Nil))
     }
 
+    // Turn extractions info flatMaps
     def doExtractions(next: Transformation) = Transformation("extractions") { t =>
       val reversed = t.shard.reverse
       def doTransformation(
@@ -501,153 +491,6 @@ class FlowMacros(val c: blackbox.Context){
 
         transforms.all.transform(nonEmptyBody)
 
-        // val M = weakTypeOf[M]
-        // val companion = M.typeSymbol.companion
-        // def unit(tree: Tree) = q"implicitly[Constructor[$M]].create($tree)"
-
-      //   def transformExtract(tree: Tree): (List[Tree], Tree) = {
-      //     object transformer extends Transformer {
-      //       val extracted = collection.mutable.MutableList[Tree]()
-      //       override def transform(tree: Tree) = {
-      //         val t = tree match {
-      //           case t2@q"$flowContextUsage.?[$tpe]($expr)" if flowContextUsage.symbol == flowContext.symbol => 
-      //             val name = c.freshName
-      //             val (statements, texpr) = transformExtract(expr)
-      //             val v = q"val ${TermName(name)}: $tpe = $flowContextUsage.?[$tpe]($texpr)"
-      //             extracted ++= (statements :+ v)
-      //             q"${Ident(TermName(name))}"
-
-      //           case other => other
-      //         }
-      //         super.transform(t)
-      //       }
-      //     }
-      //     val res = transformer.transform(tree)
-      //     (transformer.extracted.to[List], res)
-      //   }
-
-      //   // untypechecking only seems to work reliably upfront.
-      //   // As a consequence, all of the following processes pairs of
-      //   // corresponding typed trees and untyped tres so both variants
-      //   // are available as needed. The untyped trees are used
-      //   // to build the eventual resulting tree. The typed trees
-      //   // are used to examine the type information of the input
-      //   // trees.
-      //   // value names ending in T mean typed trees from here on.
-      //   // The same names excluding the T mean the corresponding untyped trees
-      //   (e, c.untypecheck(e)) match {
-      //     case (
-      //       q"""..$statementsT; $resultT""", // <- 
-      //       q"""..$statements;  $result """  // <- 
-      //     ) =>
-      //       /* The following folds over the comprehension statements from top to bottom.
-      //        * Every step results in a new scope (the names of values bound by the
-      //        * comprehension at this point) and a context, which is a function that takes
-      //        * a tree and embeds it into another tree that binds the required
-      //        * values. The context is effectively a continuation, which allows
-      //        * folding forward throw the flat of statements while allowing
-      //        * the following statment decide how it handles the continuation of previous
-      //        * statments. This allows closing the iteration and assigning the result to
-      //        * a val for example, in order to call contextual transformers
-      //        * (sortBy, filter, ...) on the iteration up to this point.
-      //       */
-
-      //       val (additionalStatementsT, expressionT) = transformExtract(resultT)
-      //       val (additionalStatements, expression) = transformExtract(result)
-
-      //       val statementPairs = (
-      //         (statementsT.flatMap(transformExtract(_) match{ case (list,one) => list :+ one }) ++ additionalStatementsT)
-      //         zip
-      //         (statements.flatMap(transformExtract(_) match{ case (list,one) => list :+ one }) ++ additionalStatements)
-      //       )
-
-      //       val (_, continuation) = statementPairs.foldLeft(
-      //         ( List[(TermName,Tree)](), identity[Tree] _ )
-      //       ){
-      //         case (
-      //           ( scope, context ),
-      //           (
-      //             valdefT @ q"val $nameT: $tpeT = $flowContextUsage.?[$t]($mT)",
-      //             valdef  @ q"val $name : $tpe  = $flowContextUsageT.?[$tT]($m )"
-      //           )
-      //         )  if flowContextUsage.symbol == flowContext.symbol && flowContextUsageT.symbol == flowContext.symbol
-      //          =>
-      //           val param = q"val $name: ${TypeTree()}"
-      //           (
-      //             // omit generated aliases from being captured - users can't refer to them anyways
-      //             if(name.toString.startsWith("fresh$macro$")) scope else ((name, tpeT) :: scope),
-      //             continue => context(q"$m.flatMap( $param => $continue )")
-      //           )
-      //         case ( ( scope, context ), (q"$ctxT.!($transformerT)", q"$ctx.!($transformer)") ) if ctx.symbol == flowContext.symbol =>
-      //           val boundNames = scope.map(_._1).map(Ident.apply _)
-
-      //           object ReplaceFlowScope extends Transformer {
-      //             override def transform(tree: Tree) = {
-      //               val t = tree match {
-      //                 case t@q"($arg) => $expr" if arg.tpt.tpe != null && arg.tpt.tpe =:= typeOf[FlowContext] =>
-      //                   val pattern =  pq"(..${scope.map(_._1)})"
-      //                   // FIXME: can we move the extractor below into the argument?
-      //                   q"""{
-      //                     arg =>
-      //                       val $pattern = arg
-      //                       ${transform(expr)}
-      //                   }"""
-      //                 case other => other
-      //               }
-      //               super.transform(t)
-      //             }
-      //           }
-      //           val captured = context{
-      //             unit(q"(..$boundNames)")
-      //           }
-      //           val params = scope.map{ case(name, tpe) => ValDef(Modifiers(Flag.PARAM),name,tpe,EmptyTree) }
-      //           val before = transformer match {
-      //             case q"($arg) => $expr" => 
-      //               val q"$_ val $name: $_ = $_" = arg
-      //               q"""
-      //                 val $name = $captured
-      //                 $expr
-      //               """
-      //             case q"$other" => q"$other($captured)"
-      //           }
-      //           //println("before:"+before)
-      //           val closed =  ReplaceFlowScope.transform(before)
-      //           //println("closed:"+closed)
-      //           (scope, continue => {
-      //             val func =
-      //               if(params.size > 1)
-      //                 q"((..$params) => $continue).tupled"
-      //               else
-      //                 q"((..$params) => $continue)"
-      //             val name = c.freshName
-      //             q"""
-      //               val ${TermName(name)} = $closed
-      //               ${Ident(TermName(name))}.flatMap{
-      //                 $func            
-      //               }
-      //             """
-      //           })
-      //         case (
-      //           ( scope, context ),
-      //           (
-      //             valdefT @ q"val $nameT: $tpeT = $otherT",
-      //             valdef  @ q"val $name : $tpe  = $other"
-      //           )
-      //         ) =>
-      //           (
-      //             (name, tpeT) :: scope,
-      //             continue => context(q"$valdef; $continue")
-      //           )
-      //         case ( ( scope, context ), (otherT, other) ) => 
-      //           (scope, continue => context(q"$other; $continue"))
-      //       }
-      //       val res = continuation(unit(expression))
-      //       //println(e)
-      //       //println(res)
-      //       //println(showRaw(res))
-      //       RemoveFlowContextTypeAnnotations.transform(res)
-      //   }
-      // case x => throw new Exception(x.toString)
     }
   }
 }
