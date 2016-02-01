76f2b805a7556e674506ab3588d42be24d869120
Chris Hodapp <chodapp@rocketlawyer.com>
2016-02-01 02:57:00 -0800

    Work on normalizer

--- src/main/scala/flow.scala
+++ src/main/scala/flow.scala
@@ -181,49 +181,300 @@ class FlowMacros(val c: blackbox.Context){
 
   class Transformations(
     contextName: TermName,
-    liftM: Tree => Tree
+    M: Type,
+    liftM: Tree => Tree,
+    debug: Boolean = true
   ) {
-    val doContextTransforms: Tree => Tree = identity
-    val normalize: Tree => Tree = identity
 
-    val doExtractions: Tree => Tree = { t =>
-      val q"..${statements: List[Tree]}" = t
-      val (processed, returned) = {
-        if (statements.isEmpty) (List.empty[Tree], q"()": Tree)
-        else (statements.init: List[Tree], statements.last: Tree)
-      }
-      processed.foldRight(liftM(returned)) { (statement, done) =>
-        statement match {
-          case v@ValDef(_, `contextName`, _, _) =>
-            c.abort(v.pos, s"Duplicate definition for $contextName")
-          case v@DefDef(_, `contextName`, _, _, _, _) =>
-            c.abort(v.pos, s"Duplicate definition for $contextName")
-          case ValDef(
-            mods, name, valTpe, q"$contextName.?[$paramTpe]($extracted)"
-          ) if !mods.hasFlag(Flag.LAZY) =>
-            val param = TermName(c.freshName("param"))
-            q"""
-              $extracted.flatMap { ($param: $paramTpe) =>
-                $mods val $name: $valTpe = $param
-                $done
-              }
-            """
-          case q"$contextName.?[$paramTpe]($extracted)" =>
-            val param = TermName(c.freshName("param"))
-            q"""
-              $extracted.flatMap { ($param: $paramTpe) =>
-                $done
-              }
-            """
-          case other => q"$other; $done"
+    case class Transformation(description: String)(_transform: Tree => Tree) {
+      def transform(t: Tree): Tree = {
+        if (debug) {
+          println(s"==============")
+          println(s"$description input:\n"++showCode(t))
+          println(s"==============")
         }
+        val res = _transform(t)
+        if (debug) {
+          println(s"==============")
+          println(s"$description output:\n"++showCode(res))
+          println(s"==============")
+        }
+        res
       }
     }
 
-    val transform: Tree => Tree =
-      doContextTransforms andThen normalize andThen doExtractions
-  }
+    val doNothing = new Transformation("identity")(identity) {
+      override def transform(t: Tree) = t
+    }
 
+    def untypecheck(next: Transformation) =
+      Transformation("untypecheck") { t =>
+        next.transform(c.untypecheck(t))
+      }
+
+    def doImplicitExtracts(next: Transformation) =
+      Transformation("implicit extracts") { t =>
+        def doTransformation(
+          pending: List[Tree],
+          done: List[Tree]
+        ): Tree = pending match {
+          case Nil => q"..$done"
+          case last :: Nil =>
+           val results = done :+ last
+            q"..$results"
+          case statement :: rest =>
+            val t = if (statement.tpe.typeSymbol == M.typeSymbol) {
+              val tpe = statement.tpe.baseType(M.typeSymbol)
+              Extract(statement, TypeTree(tpe.typeArgs(0)))
+            } else statement
+            doTransformation(rest, done :+ t)
+        }
+        next.transform(doTransformation(t.shard, Nil))
+      }
+
+    def doContextTransforms(next: Transformation) =
+      Transformation("context transform") { t =>
+
+        sealed trait ScopeOp
+        case class DefineVal(valDef: ValDef) extends ScopeOp
+        case class DefineObject(modDef: ModuleDef) extends ScopeOp
+        case class DefineDef(defDef: DefDef) extends ScopeOp
+        case class ImportSomething(imported: Import) extends ScopeOp
+
+        // For now, we'll ignore local classes and traits, but we should support them
+        case class DefineClass(defined: ClassDef) extends ScopeOp
+
+        def doContextTransform(
+          trees: List[Tree],
+          scopeOps: List[ScopeOp],
+          done: List[Tree]
+        ): Tree = trees match {
+          case Nil => next.transform(done.unify)
+          case (v: ValDef) :: rest if !v.mods.hasFlag(Flag.ARTIFACT) =>
+            doContextTransform(
+              rest,
+              scopeOps :+ DefineVal(v),
+              done :+ v
+            )
+          case (d: DefDef) :: rest =>
+            doContextTransform(
+              rest,
+              scopeOps :+ DefineDef(d),
+              done :+ d
+            )
+          case (m: ModuleDef) :: rest =>
+            doContextTransform(
+              rest,
+              scopeOps :+ DefineObject(m),
+              done :+ m
+            )
+          case (i: Import) :: rest =>
+            doContextTransform(
+              rest,
+              scopeOps :+ ImportSomething(i),
+              done :+ i
+            )
+          case q"""
+            $contextName.! { ($nme1: $tpe1) =>
+              $nme.$op { ..$stats; ($nme2: $tpe2) => $expr }
+            }
+          """ :: rest =>
+            val body = q"..$stats; $expr"
+            val prefix = next.transform(q"..$done")
+            val paramName = TermName(c.freshName("param"))
+            val transformed = q"$prefix.$op(($paramName: Any) => $body)"
+            val continued = doContextTransform(rest, Nil, Nil)
+            q"$transformed.flatMap { (paramName: Any) => $continued }"
+          case other :: rest =>
+            doContextTransform(
+              rest,
+              scopeOps,
+              done :+ other
+            )
+        }
+        doContextTransform(t.shard, Nil, Nil)
+      }
+
+    def hasExtracts(t: Tree): Boolean = {
+      var foundExtraction = false
+      new Traverser {
+        override def traverse(t: Tree): Unit = {
+          if (foundExtraction || isExtract(t)) foundExtraction = true
+          else super.traverse(t)
+        }
+      }.traverse(t)
+      foundExtraction
+    }
+
+    def noExtracts(t: Tree): Boolean = !hasExtracts(t)
+
+    def isExtract(t: Tree): Boolean = t match {
+      case q"$contextName.?[$tpe]($body)" => true
+      case other => false
+    }
+
+    def notExtract(t: Tree): Boolean = !isExtract(t)
+
+    object Extract {
+      def apply(body: Tree, tpe: Tree): Tree = q"$contextName.?[$tpe]($body)"
+      def unapply(t: Tree): Option[(Tree, Tree)] = t match {
+        case q"$contextName.?[$tpe]($body)" => Some((body, tpe))
+        case other => None
+      }
+    }
+
+    implicit class TreeOps(t: Tree) {
+      def shard: List[Tree] = {
+        val q"..$result" = t
+        result
+      }
+    }
+
+    implicit class ListTreeOps(t: List[Tree]) {
+      def unify: Tree = q"..$t"
+    }
+
+    def normalize(next: Transformation): Transformation = Transformation("normalize") { t =>
+
+      def doNormalize(pending: List[Tree], done: List[Tree]): Tree = pending match {
+
+        case Nil => done.unify
+
+        case first :: rest if noExtracts(first) =>
+          doNormalize(rest, done :+ first)
+
+        case h :: rest if hasExtracts(h) && !h.isInstanceOf[ValDef @unchecked] =>
+          val nme = TermName(c.freshName("anon"))
+          val preserveFinalExpress = {
+            if (rest.isEmpty) List(q"$nme")
+            else Nil
+          }
+          doNormalize(q"val $nme = $h" +: preserveFinalExpress ::: rest, done)
+
+        case q"..$body" :: rest if body.size > 1 && (hasExtracts(body.unify)) =>
+          doNormalize(
+            rest,
+            Extract(normalize(next).transform(body.unify), TypeTree()).shard
+          )
+
+        case (v@q"$mods val $nme: $tpe1 = ${Extract(e, tpe2)}") :: rest
+            if hasExtracts(e) =>
+          val tmpNme = TermName(c.freshName("anon"))
+          val split = List(
+            q"val $tmpNme = $e",
+            q"val nme: $tpe1 = ${Extract(q"$tmpNme", tpe2)}"
+          )
+          doNormalize(split ++ rest, done)
+
+        case (v@q"$mods val $nme: $tpe = ${Extract(e, _)}") :: rest if noExtracts(e) =>
+          doNormalize(rest, done :+ v)
+
+        case q"$mods val $nme: $tpe = $pre.$op[..$targs](...$vargs)" :: rest
+            if hasExtracts(pre) =>
+          val tmpNme = TermName(c.freshName("anon"))
+          val split = List(
+            q"val $tmpNme = $pre",
+            q"$mods val $nme: $tpe = $tmpNme.$op[..$targs](...$vargs)"
+          )
+          doNormalize(split ++ rest, done)
+
+        case q"$mods val $nme: $tpe = $pre match { case ..$body }" :: rest
+            if hasExtracts(pre) =>
+          val tmpNme = TermName(c.freshName("anon"))
+          val split = List(
+            q"val $tmpNme = $pre",
+            q"$mods val $nme: $tpe = $tmpNme match { case ..$body }"
+          )
+          doNormalize(split ++ rest, done)
+
+        case q"$mods val $nme: $tpe = $pre.$op[..$targs](...$vargs)" :: rest
+            if vargs.exists(_.exists(hasExtracts)) =>
+          val (newArgss, defs) = {
+            vargs.foldLeft((List.empty[List[Tree]], List.empty[ValDef])) {
+              case ((processedArgss, defs), newArgs) =>
+                val (p, d) =
+                  newArgs.foldLeft(List.empty[Tree], List.empty[ValDef]) {
+                    case ((processedArgs, defs), newArg) =>
+                      if (hasExtracts(newArg)) {
+                        val tmpNme = TermName(c.freshName("arg"))
+                        newArg match {
+                          case AssignOrNamedArg(lhs, rhs) =>
+                            (
+                              processedArgs :+ AssignOrNamedArg(lhs, q"$tmpNme"),
+                              defs :+ q"val $tmpNme = $rhs"
+                            )
+                          case other =>
+                            (
+                              processedArgs :+ q"$tmpNme",
+                              defs :+ q"val $tmpNme = $other"
+                            )
+                        }
+                      } else (processedArgs :+ newArg, defs)
+                  }
+                (processedArgss :+ p, defs ++ d)
+            }
+          }
+
+          doNormalize(
+            defs ::: q"$mods val $nme: $tpe = $pre.$op[..$targs](...$newArgss)" :: rest,
+            done
+          )
+      }
+      next.transform(doNormalize(t.shard, Nil))
+    }
+
+    def doExtractions(next: Transformation) = Transformation("extractions") { t =>
+      val reversed = t.shard.reverse
+      def doTransformation(
+        remaining: List[Tree],
+        done: List[Tree]
+      ): Tree = remaining match {
+        case Nil => done.unify
+        case (v@ValDef(_, `contextName`, _, _)) :: rest =>
+          c.abort(v.pos, s"Duplicate definition for $contextName")
+        case (v@DefDef(_, `contextName`, _, _, _, _)) :: rest =>
+          c.abort(v.pos, s"Duplicate definition for $contextName")
+        case ValDef(
+          mods, name, valTpe, Extract(extracted, tpe)
+        ) :: rest if !mods.hasFlag(Flag.LAZY) =>
+          val param = TermName(c.freshName("param"))
+          doTransformation(
+            rest,
+            q"""
+              $extracted.flatMap { ($param: $tpe) =>
+                $mods val $name: $valTpe = $param
+                ..$done
+              }
+            """.shard
+          )
+        case q"$contextName.?[$paramTpe]($extracted)" :: rest =>
+          val param = TermName(c.freshName("param"))
+          doTransformation(
+            rest,
+            q"""
+                $extracted.flatMap { ($param: $paramTpe) =>
+                  $done
+                }
+            """ +: done
+          )
+        case other :: rest =>
+          doTransformation(
+            rest,
+            other +: done
+          )
+      }
+      doTransformation(liftM(reversed.head) :: reversed.tail, Nil)
+    }
+
+    val all = List(
+      doImplicitExtracts(_),
+      untypecheck(_),
+      doContextTransforms(_),
+      normalize(_),
+      doExtractions(_)
+    ).foldRight(doNothing)((t, next) => t(next))
+
+  }
 
   /** like identity but prints desugared code and tree */
   def debugMacro(tree: Tree): Tree = {
@@ -235,15 +486,20 @@ class FlowMacros(val c: blackbox.Context){
     comprehension match {
       case q"($contextName: $tpe) => $body" =>
 
+        val nonEmptyBody = body match {
+          case EmptyTree => q"()"
+          case nonEmpty => nonEmpty
+        }
+
+        val debug = sys.props.get("flat_debug").map(_.toBoolean).getOrElse(false)
+
         val M = weakTypeOf[M].typeConstructor
         val liftM: Tree => Tree = t =>
           q"_root_.org.cvogt.flow.Constructor[$M].create($t)"
 
-        val untyped = c.untypecheck(body)
+        val transforms = new Transformations(contextName, M, liftM, debug)
 
-        val transformer = new Transformations(contextName, liftM)
-
-        transformer.transform(untyped)
+        transforms.all.transform(nonEmptyBody)
 
         // val M = weakTypeOf[M]
         // val companion = M.typeSymbol.companion
